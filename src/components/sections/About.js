277fdfea6db5ff6d95e8e62c17ebf8ab713ab5bd
bert guy <bertcaivano@gmail.com>
2020-11-10 22:31:59 -0800

    updated about me section

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -73,14 +73,7 @@ function About() {
       <div className="container">
         <Pagetitle title="About Me" />
         <div className="row">
-          <div className="col-md-3">
-            <div className="text-center text-md-left">
-              <img src={aboutContent.avatarImage} alt={aboutContent.name} />
-            </div>
-            <div className="spacer d-md-none d-lg-none" data-height="30"></div>
-          </div>
-
-          <div className="col-md-9 triangle-left-md triangle-top-sm">
+          <div className="col-md-12 triangle-top-sm">
             <div className="rounded bg-white shadow-dark padding-30">
               <div className="row">
                 <div className="col-md-6">
